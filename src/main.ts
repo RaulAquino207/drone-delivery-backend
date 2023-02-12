0640257167088a357d769878a2aaf8a05ff9c319
Raul Aquino <aquinoraul207@gmail.com>
2023-02-12 12:34:06 -0300

    added swagger and settings via environment variable

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  
+  if(process.env.NODE_ENV == 'development') {
+    const config = new DocumentBuilder()
+    .setTitle('Drone Delivery')
+    .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
+    Logger.log(`Documentation available at http://localhost:${process.env.PORT}/api`)
+  }
+
+  await app.listen(process.env.PORT || 3000, () => {
+    Logger.log(`[PORT]: ${process.env.PORT || 3000}`);
+  });
 }
 bootstrap();
