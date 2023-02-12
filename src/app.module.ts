83b320f9650b693aedeb781ff67e484d924dcaad
Raul Aquino <aquinoraul207@gmail.com>
2023-02-12 14:54:58 -0300

    generated the resource of some of the database entities in order to create data in the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DroneModule } from './modules/drone/drone.module';
+import { WarehouseModule } from './modules/warehouse/warehouse.module';
+import { UserModule } from './modules/user/user.module';
+import { AdminModule } from './modules/admin/admin.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), DroneModule, WarehouseModule, UserModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
